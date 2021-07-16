3718f9a94590e1fb3b11291d854eb62ef20d5277
hamza887 <hamzabutt887>
2021-07-16 12:29:51 -0400

    working on reviews

--- react-bookstop/src/App.js
+++ react-bookstop/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';   //minimizing bootstrap use 
 import Navbar from './components/NavBar';
 import SearchForm from './components/SearchForm';
+import Review from './components/Reviews';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <SearchForm/>
+      <Review/>
+
       
 
     </div>
