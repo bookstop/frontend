dddb4f42db18782bcac1ef0ec675ddf2f032a74f
adinahanna <adina.weinberg@gmail.com>
2021-07-16 17:03:18 -0500

    building out ReadBook component

--- react-bookstop/src/components/ReadBook/ReadBook.js
+++ react-bookstop/src/components/ReadBook/ReadBook.js
@@ -1,5 +1,18 @@
+import { useContext } from 'react';
 
 const ReadBook = () => {
+    const userContext = useContext(UserContext);
+
+    const getBook = async () => {
+        try {
+            const API_ENDPOINT = `http://localhost:4000/read-books/${userContext.user.book._id}`;
+            const response = await fetch(API_ENDPOINT);
+            const data = await response.json();
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
 
