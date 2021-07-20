49086b5e0f8c7219fb31a346b50eb3989c746ccb
chicago29094 <harry.anastopoulos@telusys.net>
2021-07-20 00:15:03 -0500

    Update Styles

--- react-bookstop/src/components/Home/index.js
+++ react-bookstop/src/components/Home/index.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import ReadBooks from "../ReadBooks";
 import WishList from "../WishList";
 import SearchForm from "../SearchForm";
 
 const Home = () => {
 
+const [haveNewBooks, setHaveNewBooks] = useState(false);
+
     return (
         <div>
             <ReadBooks />
             <WishList />
-            <SearchForm/>
+            <SearchForm  haveNewBooks={haveNewBooks} setHaveNewBooks={setHaveNewBooks} />
         </div>
     )
 };
